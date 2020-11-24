2905ec9705e60200972ed6c1ea038a5046f49d59
copihuexnegro <fmicheam@gmail.com>
2020-11-24 17:49:46 -0300

    primer commit

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Hotel Ecomusic </title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Reserva en Hotel Ecomusic"   />
       </Head>
 
       <main className={styles.main}>
